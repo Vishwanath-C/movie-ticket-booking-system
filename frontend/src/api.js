3feae91c74f05594063cacd2df4da7e0ac421147
Vishwanath-C <vishwanath.kardvg@gmail.com>
2025-12-15 00:29:45 +0530

    commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,7 +4,8 @@ export const API_URL = import.meta.env.VITE_API_URL;
 
 const apiClient = axios.create({
   // baseURL: API_URL,
-  baseURL: "http://127.0.0.1:8080",
+//  baseURL: "http://127.0.0.1:8080",
+  baseURL : import.meta.env.VITE_API_URL,
 });
 
 export default apiClient;
