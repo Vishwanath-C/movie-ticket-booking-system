52f9312b92dfaca08d59f65532e3ea17216159dc
Vishwanath-C <vishwanath.kardvg@gmail.com>
2025-12-15 00:11:26 +0530

    commit

--- backend/src/main/java/com/example/MovieTicketBooking/config/security/SecurityConfig.java
+++ backend/src/main/java/com/example/MovieTicketBooking/config/security/SecurityConfig.java
@@ -111,6 +111,7 @@ public class SecurityConfig
         var c = new CorsConfiguration();
         c.setAllowedOriginPatterns(List.of("http://localhost:*",
                 "http://127.0.0.1:*"));
+        c.addAllowedOrigin("https://movie-ticket-booking-mtb.netlify.app/");
         c.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         c.setAllowedHeaders(List.of("*"));
         c.setAllowCredentials(true);
